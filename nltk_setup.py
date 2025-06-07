48346b47530732d6beac3101632667e42ad6a7d1
Behiye Nilay OGULTEKIN <111127979+BehiyeNilayOGULTEKIN@users.noreply.github.com>
2025-06-07 16:48:35 +0300

    Update nltk_setup.py

--- nltk_setup.py
+++ nltk_setup.py
@@ -1,5 +1,5 @@
 import nltk
-nltk.download('punkt')
+nltk.download('punkt', download_dir='nltk_data')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
